7761926d4b9ccd5a6af6249aa4b453edbf678d5d
venkatvkpt <venkatvkpt@gmail.com>
2018-09-15 09:51:18 +0530

    program one

---
+++ Programming Practice/src/in/programs/reversenumber/ReverseGivenNumber.java
@@ -0,0 +1,28 @@
+package in.programs.reversenumber;
+
+import java.util.Scanner;
+
+public class ReverseGivenNumber {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner givenNumber = new Scanner(System.in);
+		System.out.print("Enter Number: ");
+		int number = Integer.parseInt(givenNumber.nextLine());
+		System.out.println("Reverse Number: "+numberReversing(number));
+	}
+	
+	@SuppressWarnings("unused")
+	private static int numberReversing(int inputNumber)
+	{
+		int tempNumber = 0,outputNumber = 0;
+		while(inputNumber >0) {
+			tempNumber = inputNumber % 10;
+			outputNumber = outputNumber *10 + tempNumber;
+			inputNumber = inputNumber / 10;
+		}
+		
+		return outputNumber;	
+	}
+
+}
